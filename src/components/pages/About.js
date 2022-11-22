edfd1967151ff2dfea15995db3272373920fd704
pjay7883 <89178187+pjay7883@users.noreply.github.com>
2022-11-22 14:12:33 +1300

    new change

---
+++ src/components/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Page</h1>
+        <p className="lead">
+          There's this notion that to grow a business, you have to be ruthless. But we know there's a better way to grow. One where what's good for the bottom line is also good for customers. We believe businesses can grow with a conscience, and succeed with a soul — and that they can do it with inbound. That's why we've created an ecosystem uniting software, education, and community to help businesses grow better every day.
+        </p>
+
+        <p className="lead">
+          An 'About Us' page is a spot for your founding story, a place to show off your business wins, and a sales page that answers the most pressing question new customers have about your business.
+
+          What an 'About Us' page should be is a goal-oriented sales page, one that focuses on highlighting the biggest selling points of your story and brand at the top of the page, making a strong first impression on curious customers
+
+          In Shopify’s customer trust research, we found that shoppers navigate to an About Us page to learn more about the brand and the people behind the products. Your 'About Page' should address those two curiosities shoppers have to help with decision making.
+
+          Shoppers are also interested in a company’s mission. They’ll use the About Us page to determine if they share core values with the business and to decide if they want to shop with the business or not.
+
+          In contrast to a landing page, your About Us page is the ideal place to accommodate a number of objectives:
+        </p>
+        <p className="lead">Made By Jaykumar Patel</p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
